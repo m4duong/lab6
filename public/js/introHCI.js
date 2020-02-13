1b4898e2a339b86a249e87b37c0062a1bb524991
Michelle Duong <m4duong@ucsd.edu>
2020-02-13 11:46:41 -0800

    ajax and facebook lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,7 +10,6 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$('.project a').click(addProjectDetails);
-
 	// $('#colorBtn').click(randomizeColors);
 }
 
@@ -27,4 +26,15 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, callBackFn)
+}
+
+function callBackFn(result) {
+	var projectHTML =  '<p>' + result['title'] + '</p>' +
+    '<p>' + result['date'] +'</p>' +
+	'<img src="' + result['image'] + '" class="detailsImage">' +
+	'<p>' + result['summary'] + '</p>';
+	
+	$("#project" + result['id'] + " div.details").html(projectHTML); 
 }
